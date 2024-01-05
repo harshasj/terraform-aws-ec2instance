193965ceb2a580cc30c6d7d742e18edbf02e1d63
harshasj <36756863+harshasj@users.noreply.github.com>
2024-01-05 08:51:18 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,22 +2,6 @@ variable "instance_ami" {
   type = string
 }
 
-//variable "instance_count" {
-//  type = string
-//}
-
 variable "type_instance" {
-  type = string
-}
-
-variable "key_name" {
-  type = string
-}
-
-variable "subnet_id" {
-type = string
-}
-
-variable "vpc_id" {
   type = string
 }
