5586ce7cd1fd8265a91ade2013aa942010006437
harshasj <36756863+harshasj@users.noreply.github.com>
2024-01-05 12:46:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami                    = var.instance_ami
   instance_type          = var.type_instance
-  user_data              = <<-EOF
+ /*   user_data              = <<-EOF
     #!/bin/bash
     sudo apt update -y
     sudo apt install git -y
@@ -9,7 +9,7 @@ resource "aws_instance" "web" {
     sudo systemctl start httpd
     echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
     EOF 
-  /*     
+     
 
   */
   tags = {
