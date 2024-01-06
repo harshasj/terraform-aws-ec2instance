ba6a5f12469ddbb96981d89786a9536443313767
harshasj <36756863+harshasj@users.noreply.github.com>
2024-01-06 13:24:49 +0530

    new update

--- main.tf
+++ main.tf
@@ -5,14 +5,45 @@ resource "aws_instance" "web" {
     #!/bin/bash
     sudo apt update -y
     sudo apt install git -y
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+    sudo apt install nginx -y
     EOF 
      
 
   
   tags = {
     Name = "HelloWorld"
+  }
+}
+
+resource "aws_security_group" "allow_all_traffic" {
+  name        = "allow_all_traffic"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    description      = "all traffic "
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description      = "all traffic "
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_all_traffic"
   }
 }
